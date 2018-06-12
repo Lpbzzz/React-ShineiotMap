2a814226ad223a01ec40b25b89a6a0048d2959aa
lpb <462570646@qq.com>
2018-06-12 10:20:35 +0800

    first

---
+++ src/components/infowindow.md
@@ -0,0 +1,21 @@
+# InfoWindow
+信息窗口组件，可参考官网对于一些[属性](http://lbsyun.baidu.com/cms/jsapi/reference/jsapi_reference.html#a3b7)
+可配置属性：
+- position
+- width
+- height
+- maxWidth
+- offset
+- title
+- enableAutoPan
+- enableCloseOnClick
+- enableMessage
+- message
+- events
+
+事件options：
+- close
+- open
+- maximize
+- restore
+- clickclose
---
+++ src/overlay/CustomOverlay.js
@@ -0,0 +1,46 @@
+// 自定义覆盖物
+function CustomOverlay(point, content, offset) {
+    this._point = point;
+    this.content = content;
+    this.offset = offset || new BMap.Size(0, 0);
+}
+
+CustomOverlay.prototype = new BMap.Overlay();
+
+CustomOverlay.prototype.initialize = function(map){
+    this._map = map;
+    var div = this._div = document.createElement("div");
+    div.setAttribute('tag', 'customoverlay');
+    div.style.position = "absolute";
+    div.style.zIndex = BMap.Overlay.getZIndex(this._point.lat);
+    div.addEventListener('touchstart', function (e) {
+        e.stopPropagation();
+        console.log('touchstart');
+    });
+    /*
+    div.addEventListener('touchmove', function (e) {
+        e.stopPropagation();
+        console.log('touchmove');
+    });
+    */
+    div.addEventListener('touchend', function (e) {
+        e.stopPropagation();
+        console.log('touchend');
+    });
+    if (Object.prototype.toString.call(this.content) == "[object String]") {
+        div.innerHTML = this.content;
+    } else {
+        div.appendChild(this.content);
+    }
+    map.getPanes().labelPane.appendChild(div);
+    return div;
+}
+
+CustomOverlay.prototype.draw = function(){
+    var map = this._map;
+    var pixel = map.pointToOverlayPixel(this._point);
+    this._div.style.left = pixel.x + this.offset.width + "px";
+    this._div.style.top  = pixel.y + this.offset.height + "px";
+}
+
+export default CustomOverlay;
